bb2333135d2d3f92157a3c3901e664c7f734c537
Lock309 <163652054+Lock309@users.noreply.github.com>
2026-06-03 14:28:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -258,13 +258,6 @@
                                 safeWorkerSend(worker, { type: "destroy" });
                             }
                             session.workers = [];
-
-                            if (session.scanWorkers) {
-                                for (const worker of session.scanWorkers) {
-                                    safeWorkerSend(worker, { type: "destroy" });
-                                }
-                                session.scanWorkers = [];
-                            }
                         }
 
                         break;
